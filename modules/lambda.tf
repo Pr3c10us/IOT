8126e09fcc8c576216f0dd4220104f984f8887ec
owopr <owo.pre.eno@gmail.com>
2024-11-17 10:14:22 +0100

    fifth

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -1,16 +1,37 @@
 # Null resource to run the build script in the lambda folder
-resource "null_resource" "build_lambda" {
-  # Add triggers to rebuild when source code changes
+# resource "null_resource" "build_lambda" {
+#   # Add triggers to rebuild when source code changes
+#   triggers = {
+#     source_code_hash = filemd5("${path.module}/lambda/lambda_function.py")
+#     requirements_hash = filemd5("${path.module}/lambda/requirements.txt")
+#     build_script_hash = filemd5("${path.module}/lambda/build_lambda.sh")
+#   }
+#
+#   provisioner "local-exec" {
+#     command     = "chmod +x ./lambda/build_lambda.sh && ./lambda/build_lambda.sh"
+#     working_dir = path.module
+#   }
+# }
+
+# Install dependencies if needed
+resource "null_resource" "install_dependencies" {
   triggers = {
-    source_code_hash = filemd5("${path.module}/lambda/lambda_function.py")
     requirements_hash = filemd5("${path.module}/lambda/requirements.txt")
-    build_script_hash = filemd5("${path.module}/lambda/build_lambda.sh")
   }
 
   provisioner "local-exec" {
-    command     = "chmod +x ./lambda/build_lambda.sh && ./lambda/build_lambda.sh"
-    working_dir = path.module
+    command = "pip install --target ${path.module}/lambda -r ${path.module}/lambda/requirements.txt"
   }
+}
+
+# Create zip with dependencies included
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda/lambda_function.zip"
+  excludes    = ["lambda_function.zip", "build_lambda.sh", "requirements.txt"]
+
+  depends_on = [null_resource.install_dependencies]
 }
 
 # Post-Delivery Lambda Function
@@ -19,8 +40,8 @@ resource "aws_lambda_function" "post_delivery_lambda" {
   role             = aws_iam_role.lambda_post_delivery_role.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
-  filename         = "${path.module}/lambda/lambda_function.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function.zip")
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
@@ -38,6 +59,4 @@ resource "aws_lambda_function" "post_delivery_lambda" {
     Environment = "Production"
     ManagedBy   = "Terraform"
   }
-
-  depends_on = [null_resource.build_lambda]
 }
